e8c4e7801a30af8802d9fc77f37e0f5f6e2a9d31
0xNaN <enzodicicco@gmail.com>
2015-11-23 15:52:20 +0100

    refactoring test

--- apps/kademlia/test/peer.hrl
+++ apps/kademlia/test/peer.hrl
@@ -1,12 +1,19 @@
 -include_lib("eunit/include/eunit.hrl").
 
+-define(setup(F), {setup, fun start/0, F}).
 
-should_store_data_test() ->
+start() ->
     Id = 1,
+    PeerPid = peer:start(Id),
+    PeerPid.
+
+peer_suite_test_() ->
+    ?setup(fun should_store_data/1).
+
+should_store_data(PeerPid) ->
     Key = mykey,
     Value = "myvalue",
 
-    PeerPid = peer:start(Id),
     peer:store({Key, Value}, PeerPid),
     RetrievalData = peer:find_value_of(Key, PeerPid),
-    ?assertEqual(Value, RetrievalData).
+    [?_assertEqual(Value, RetrievalData)].
