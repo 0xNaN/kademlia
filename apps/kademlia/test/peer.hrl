485cb077494d57d96b592ac43a127bca70586e9e
0xNaN <enzodicicco@gmail.com>
2015-11-23 15:57:33 +0100

    test that the value is updated when another store is performed

--- apps/kademlia/test/peer.hrl
+++ apps/kademlia/test/peer.hrl
@@ -8,7 +8,8 @@ start() ->
     PeerPid.
 
 peer_suite_test_() ->
-    ?setup(fun should_store_data/1).
+    [?setup(fun should_store_data/1),
+    ?setup(fun should_overwrite_data_with_same_key/1)].
 
 should_store_data(PeerPid) ->
     Key = mykey,
@@ -17,3 +18,13 @@ should_store_data(PeerPid) ->
     peer:store({Key, Value}, PeerPid),
     RetrievalData = peer:find_value_of(Key, PeerPid),
     [?_assertEqual(Value, RetrievalData)].
+
+should_overwrite_data_with_same_key(PeerPid) ->
+    Key = mykey,
+    FirstValue = "myvalue",
+    SecondValue = "updated",
+
+    peer:store({Key, FirstValue} , PeerPid),
+    peer:store({Key, SecondValue}, PeerPid),
+    RetrievalData = peer:find_value_of(Key, PeerPid),
+    [?_assertEqual(SecondValue, RetrievalData)].
