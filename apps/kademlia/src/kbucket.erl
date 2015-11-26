ee3731eff2ee7ed8f101f9e3373d7ba62ee13f43
0xNaN <enzodicicco@gmail.com>
2015-11-26 12:29:16 +0100

    introducing kbucket

---
+++ apps/kademlia/src/kbucket.erl
@@ -0,0 +1,43 @@
+-module(kbucket).
+-export([start/2]).
+-export([kbucket/3]).
+-export([put/2]).
+-export([get/2]).
+-export([distance/2]).
+
+start(OwningPeerId, K) ->
+    spawn(fun() -> kbucket(OwningPeerId, K, #{}) end).
+
+distance(FromPeerId, ToPeerId) ->
+    FromPeerId bxor ToPeerId.
+
+put(KbucketPid, PeerId) ->
+    KbucketPid ! {put, PeerId},
+    ok.
+
+get(KbucketPid, Distance) ->
+    KbucketPid ! {get, self(), Distance},
+    receive
+        {KbucketPid, Bucket} -> Bucket
+    end.
+
+kbucket(OwningPeerId, K, Contacts) ->
+    receive
+        {put, PeerId} ->
+            Distance = distance(OwningPeerId, PeerId),
+            kbucket(OwningPeerId, K, Contacts#{Distance => [PeerId]});
+        {get, FromPeer, Distance} ->
+            ResultBucket = case maps:is_key(Distance, Contacts) of
+                true -> #{Distance := Bucket} = Contacts,
+                        Bucket;
+                _    -> []
+            end,
+            FromPeer ! {self(), ResultBucket},
+            kbucket(OwningPeerId, K, Contacts);
+        _ ->
+            kbucket(OwningPeerId, K, Contacts)
+    end.
+
+-ifdef(TEST).
+-include_lib("../test/kbucket.hrl").
+-endif.
