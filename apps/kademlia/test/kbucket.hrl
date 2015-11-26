8d0fc783094090eb1be919d1e602c231fd72ad75
0xNaN <enzodicicco@gmail.com>
2015-11-26 12:36:10 +0100

    adding setup and teardown

--- apps/kademlia/test/kbucket.hrl
+++ apps/kademlia/test/kbucket.hrl
@@ -1,29 +1,33 @@
 -include_lib("eunit/include/eunit.hrl").
 
+-define(setup(F), {setup, fun start/0, fun teardown/1, F}).
+
+start() ->
+    OwningId = 2#0001,
+    K = 3,
+    KbucketPid = kbucket:start(OwningId, K),
+    KbucketPid.
+
+teardown(_) ->
+    void.
+
+peer_suite_test_() ->
+    [?setup(fun should_start_a_kbucket_process/1),
+     ?setup(fun should_create_a_bucket_if_not_exists/1),
+     ?setup(fun should_returns_an_empty_list_for_an_unknown_distance/1)].
+
 should_compute_the_distance_of_two_id_test() ->
     FirstId  = 2#0001,
     SecondId = 2#0010,
     ?assertEqual(3, kbucket:distance(FirstId, SecondId)).
 
-should_start_a_kbucket_process_test() ->
-    OwningId = 2#0001,
-    K = 3,
-    KbucketPid = kbucket:start(OwningId, K),
-    ?assert(erlang:is_pid(KbucketPid)).
+should_start_a_kbucket_process(KbucketPid) ->
+    [?_assert(erlang:is_pid(KbucketPid))].
 
-should_put_a_contact_on_its_correct_bucket_test() ->
-    K = 1,
-    OwnerPeerId = 2#0001,
-    KbucketPid = kbucket:start(OwnerPeerId, K),
-
+should_create_a_bucket_if_not_exists(KbucketPid) ->
     PeerId = 2#00011,
     ok = kbucket:put(KbucketPid, PeerId),
-    ?assertEqual([PeerId], kbucket:get(KbucketPid, 2)).
+    [?_assertEqual([PeerId], kbucket:get(KbucketPid, 2))].
 
-should_returns_an_empty_list_for_an_unknown_distance_test() ->
-    K = 1,
-    OwnerPeerId = 2#0001,
-    KbucketPid = kbucket:start(OwnerPeerId, K),
-
-    ?assertEqual([], kbucket:get(KbucketPid, 100)).
-
+should_returns_an_empty_list_for_an_unknown_distance(KbucketPid) ->
+    [?_assertEqual([], kbucket:get(KbucketPid, 100))].
