33ac988329960d96acd3f4ed7d947dd31430868b
0xNaN <enzodicicco@gmail.com>
2015-12-03 12:19:52 +0100

    adding a test for find_closest_peers on a complex network

--- apps/kademlia/test/it.erl
+++ apps/kademlia/test/it.erl
@@ -16,3 +16,39 @@ should_find_the_closest_peer_for_a_given_key_test() ->
     Key = 3,
     ClosestPeers = peer:iterative_find_peers(Peer, Key),
     ?assertEqual([ClosestPeer, MiddlePeer, Peer], ClosestPeers).
+
+should_find_the_closest_peer_for_a_given_key_in_a_complex_network_test() ->
+    K = 3,
+    PeerA = peer:start(2#0001, K),
+    PeerB = peer:start(2#0010, K),
+
+    PeerE = peer:start(2#1101, K),
+    PeerF = peer:start(2#1110, K),
+    PeerG = peer:start(2#1111, K),
+    PeerH = peer:start(2#1010, K),
+    PeerZ = peer:start(2#0111, K),
+
+    peer:check_link(PeerA, PeerB),  % B join with A
+
+    peer:check_link(PeerG, PeerA),  % G join through A
+    peer:check_link(PeerG, PeerB),  %   find its key
+                                    % know all so stop
+
+    peer:check_link(PeerH, PeerG),  % H join through G
+    peer:check_link(PeerH, PeerA),  %   find its key
+    peer:check_link(PeerH, PeerB),  %   ...
+
+    peer:check_link(PeerZ, PeerH),  % join through H
+    peer:check_link(PeerZ, PeerA),  %    find its key
+    peer:check_link(PeerZ, PeerB),
+                                    % find close bucket but no new peers
+
+    peer:check_link(PeerF, PeerE),   % F join with E
+
+    peer:check_link(PeerF, PeerZ),   % F join through Z
+    peer:check_link(PeerF, PeerH),   %   find its key
+    peer:check_link(PeerF, PeerG),   %   try to find `15` to fill kbucket 0 (empty)
+
+    Key = 3,
+    ClosestPeers = peer:iterative_find_peers(PeerE, Key),
+    ?assertEqual([PeerB, PeerA, PeerZ], ClosestPeers).
